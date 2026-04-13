dff6322931f7fe16d13385b9a30e7431a38b43c3
AadilWani2 <waniaadil211@gmail.com>
2026-04-13 20:41:05 +0530

    updated-1

---
+++ Project1/src/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        unique: [true, "Username Already Taken"],
+        required: [true, "Username is required"]
+    },
+    email:{
+        type: String,
+        unique: [true, "Email Already Exists"],
+        required: [true, "Email is required"]
+    },
+    password:{
+        type: String,
+        required: [true, "Password is required"]
+    },
+    bio:String,
+    profileImage: {
+        type:String,
+        default: "https://ik.imagekit.io/hgrxt64fo/istockphoto-1495088043-612x612.jpg"
+    }
+});
+
+const userModel = mongoose.model("newUsers", userSchema)
+
+module.exports = userModel
